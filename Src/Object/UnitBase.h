196c49c21d82cab7bfacd28a4a8050e5dd3aa35c
ratura3939 <158240290+ratura3939@users.noreply.github.com>
2024-10-10 15:16:02 +0900

    add GetTransEntity

--- Src/Object/UnitBase.h
+++ Src/Object/UnitBase.h
@@ -46,6 +46,8 @@ public:
 	//Transform‚¢‚½‚¾‚«
 	const Transform& GetTransform(void)const { return trans_;};
 
+	const Transform* GetTransformEntity(void)const { return &trans_; };
+
 	//ˆÊ’u
 	const VECTOR GetPos(void)const {return trans_.pos;};
 	//Šp“x
