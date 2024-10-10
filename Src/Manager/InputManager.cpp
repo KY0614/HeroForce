16f4f964d2917ef049ddc34f2e1f817e17d51674
井東太陽 <dekirusunsun410@gmail.com>
2024-10-10 09:55:09 +0900

    Eキー外した

--- Src/Manager/InputManager.cpp
+++ Src/Manager/InputManager.cpp
@@ -30,8 +30,6 @@ void InputManager::Init(void)
 	InputManager::GetInstance().Add(KEY_INPUT_N);
 	InputManager::GetInstance().Add(KEY_INPUT_Z);
 
-	InputManager::GetInstance().Add(KEY_INPUT_E);
-
 	InputManager::GetInstance().Add(KEY_INPUT_W);
 	InputManager::GetInstance().Add(KEY_INPUT_S);
 	InputManager::GetInstance().Add(KEY_INPUT_A);
