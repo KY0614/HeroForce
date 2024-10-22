3e55a567dfadd0156f6bbf03ed88117ea86dc6d0
nody <nody0126nody@docomo.ne.jp>
2024-10-22 15:30:36 +0900

    敵の攻撃処理追加 敵のダメージ判定、死亡判定追加 敵の歩行、歩きアニメーション追加

--- Src/Object/Enemy.cpp
+++ Src/Object/Enemy.cpp
@@ -7,60 +7,125 @@
 
 Enemy::Enemy()
 {
+	atkCdt_ = 0.0f;
+	exp_ = 1.0f;
+	hp_ = 5;
+	moveSpeed_ = 0.0f;
+	state_ = STATE::NORMAL;
 }
 
 void Enemy::Destroy(void)
 {
+	animNum_.clear();
 }
 
 void Enemy::Init(void)
 {
-	//モデル読み込み
-	trans_.SetModel(ResourceManager::GetInstance().LoadModelDuplicate(ResourceManager::SRC::ENEMY_AXEMAN));
+	//キャラ固有設定
+	SetParam();
 
 	float scl = 0.2f;
 
-	//変数の初期化
+	//共通の変数の初期化
 	trans_.scl = { scl,scl,scl };
 	trans_.pos = { 0.0f,0.0f,0.0f };
 	trans_.quaRot = Quaternion();
 	trans_.quaRotLocal = Quaternion::AngleAxis(AsoUtility::Deg2RadF(180.0f), AsoUtility::AXIS_Y);
-	
-	state_ = STATE::NML;
-
+	state_ = STATE::NORMAL;
 	atkCdt_ = 0.0f;
+	stunDef_ = 0;
+
+	//攻撃情報の初期化
+	InitSkill();
+	nowSkill_.ResetCnt();
 
 	trans_.Update();
+}
+
+void Enemy::SetParam(void)
+{
+	//モデル読み込み
+	trans_.SetModel(ResourceManager::GetInstance().LoadModelDuplicate(ResourceManager::SRC::ENEMY_AXEMAN));
+
+	//アニメーション番号の初期化
+	InitAnimNum();
+	ResetAnim(ANIM::WALK);
+
+	exp_ = 1.0f;
+	hp_ = 5;
+	stunDefMax_ = 100;
 }
 
 void Enemy::Update(void)
 {
-	//攻撃クールダウン中なら待機
-	if (atkCdt_ < ATK_CDT)state_ = STATE::NML;
-	else state_ = STATE::ATK;
+	//やられているなら何もしない
+	if (!IsAlive()) { return; }
 
 	//状態ごとのUpdate
 	switch (state_)
 	{
-	case Enemy::STATE::NML:
+	case Enemy::STATE::NORMAL:
 		UpdateNml();
 		break;
 
-	case Enemy::STATE::ATK:
+	case Enemy::STATE::ALERT_ATTACK:
+		UpdateAlert();
+		break;
+
+	case Enemy::STATE::ATTACK:
 		UpdateAtk();
 		break;
 
-	case Enemy::STATE::DEAD:
-		UpdateDead();
+	case Enemy::STATE::BREAK:
+		UpdateBreak();
 		break;
 	}
 
+	//※デバッグ　ダメージ処理
+	if (InputManager::GetInstance().IsTrgDown(KEY_INPUT_Q)) { Damage(1); }
+
 	//モデル制御
 	trans_.Update();
+
+	//アニメーション
+	Anim();
+}
+
+void Enemy::Damage(const int _damage)
+{
+	//ダメージ
+	hp_ -= _damage;
+	if (IsAlive()) { ResetAnim(ANIM::DAMAGE); }
+	else { ResetAnim(ANIM::DEATH); }
+}
+
+void Enemy::InitAnimNum()
+{
+	animNum_.emplace(ANIM::IDLE, ANIM_IDLE);
+	animNum_.emplace(ANIM::WALK, ANIM_WALK);
+	animNum_.emplace(ANIM::RUN, ANIM_RUN);
+	animNum_.emplace(ANIM::DAMAGE, ANIM_DAMAGE);
+	animNum_.emplace(ANIM::DEATH, ANIM_DEATH);
+	animNum_.emplace(ANIM::ENTRY, ANIM_ENTRY);
 }
 
 void Enemy::UpdateNml(void)
 {
+	//攻撃クールダウンが終わったなら攻撃準備開始
+	if (!IsAtkCD())
+	{
+		//攻撃クールダウンカウンタ初期化
+		atkCdt_ = 0.0f;
+
+		//攻撃警告状態に遷移
+		ChangeState(STATE::ALERT_ATTACK);
+
+		return;
+	}
+
+	//アニメーション変更(一度だけ)
+	if (moveSpeed_ == 0.0f && anim_ != ANIM::IDLE) { ResetAnim(ANIM::IDLE); }
+
 	//クールダウンカウンタ
 	atkCdt_++;
 
@@ -68,37 +133,127 @@ void Enemy::UpdateNml(void)
 	Move();
 }
 
+void Enemy::UpdateAlert(void)
+{
+	//攻撃クールダウンが終わったなら攻撃開始
+	if (!IsAtkCD())
+	{
+		//攻撃クールダウンカウンタ初期化
+		atkCdt_ = 0.0f;
+
+		//攻撃状態に遷移
+		ChangeState(STATE::ATTACK);
+
+		return;
+	}
+
+	//クールダウンカウンタ
+	atkCdt_++;
+
+	//生成が終わってないなら生成する
+	if (nowSkill_.IsFinishMotion()) 
+	{
+		//ランダムで攻撃生成
+		RandSkill();
+	}
+}
+
 void Enemy::UpdateAtk(void)
 {
+	//攻撃が終わったら状態遷移
+	if (nowSkill_.IsFinishMotion())
+	{
+		//攻撃クールダウンカウンタ初期化
+		atkCdt_ = 0.0f;
+
+		//休憩状態に遷移
+		ChangeState(STATE::BREAK);
+
+		return;
+	}
+
+	//攻撃のカウンタ
+	nowSkill_.cnt_++;
+
 	//攻撃処理
 	Attack();
 }
 
-void Enemy::UpdateDead(void)
+void Enemy::UpdateBreak(void)
+{
+	if (atkBtc_ > ATK_BTC)
+	{
+		//攻撃休憩時間の初期化
+		atkBtc_ = 0;
+
+		//通常状態に遷移
+		ChangeState(STATE::NORMAL);
+		return;
+	}
+
+	//攻撃休憩時間カウンタ
+	atkBtc_++;
+}
+
+void Enemy::Search(void)
 {
 }
 
 void Enemy::Draw(void)
 {
-	//DrawSphere3D(trans_.pos, 10.0f, 10, 0xff00ff, 0xff00ff, true);
+	//デバッグ
+	//DrawFormatString(0, 0, IsAtkCD() ? 0xffffff : 0xff0000, IsAtkCD() ? "攻撃クールダウン中" : "クールダウン終わり！！");
+	//DrawFormatString(0, 16, nowSkill_.IsFinishMotion() ? 0xffffff : 0xff0000, nowSkill_.IsFinishMotion() ? "攻撃中！！" : "攻撃終了！！");
+	DrawFormatString(0, 16, 0xffffff, "HP = %d", hp_);
+	switch (state_)
+	{
+	case Enemy::STATE::NORMAL:
+		DrawFormatString(0, 0, 0x00ff00, "現在状態:NORMAL");
+		break;
 
+	case Enemy::STATE::ALERT_ATTACK:
+		DrawFormatString(0, 0, 0x00ff00, "現在状態:ALERT_ATTACK");
+		break;
+
+	case Enemy::STATE::ATTACK:
+		DrawFormatString(0, 0, 0x00ff00, "現在状態:ATTACK");
+		break;
+
+	case Enemy::STATE::BREAK:
+		DrawFormatString(0, 0, 0x00ff00, "現在状態:BREAK");
+		break;
+	}
+
+	if (!IsAlive()) { return; }
+
+	//敵モデルの描画
 	MV1DrawModel(trans_.modelId);
+
+	//攻撃の描画
+	if (nowSkill_.IsAttack()) { DrawSphere3D(nowSkill_.pos_, 25.0f, 50.0f, 0xff0f0f, 0xff0f0f, true); }
+	else if (nowSkill_.IsBacklash_()) { DrawSphere3D(nowSkill_.pos_, 20.0f, 50.0f, 0xff0f0f, 0xff0f0f, false); }
 }
 
 void Enemy::Move(void)
 {
+	//移動量の初期化
+	moveSpeed_ = 0.0f;
+
 	//入力用
 	InputManager& ins = InputManager::GetInstance();
 
 	//移動(デバッグ)
-	if (ins.IsNew(KEY_INPUT_UP)) { ProcessMove(5.0f, 0.0f); }
-	if (ins.IsNew(KEY_INPUT_RIGHT)){ ProcessMove(5.0f, 90.0f); }
-	if (ins.IsNew(KEY_INPUT_DOWN)){ ProcessMove(5.0f, 180.0f); }
-	if (ins.IsNew(KEY_INPUT_LEFT)){ ProcessMove(5.0f, 270.0f); }
+	if (ins.IsNew(KEY_INPUT_UP)) { moveSpeed_ = WALK_SPEED; ProcessMove(moveSpeed_, 0.0f); }
+	if (ins.IsNew(KEY_INPUT_RIGHT)){ moveSpeed_ = WALK_SPEED; ProcessMove(moveSpeed_, 90.0f); }
+	if (ins.IsNew(KEY_INPUT_DOWN)){ moveSpeed_ = WALK_SPEED; ProcessMove(moveSpeed_, 180.0f); }
+	if (ins.IsNew(KEY_INPUT_LEFT)){ moveSpeed_ = WALK_SPEED; ProcessMove(moveSpeed_, 270.0f); }
 }
 
 void Enemy::ProcessMove(const float _moveSpeed, const float _deg)
 {
+	//アニメーション変更(一度だけ)
+	if (anim_ != ANIM::WALK) { ResetAnim(ANIM::WALK); }
+
 	//方向転換
 	Turn(_deg, AsoUtility::AXIS_Y);
 
@@ -118,51 +273,33 @@ void Enemy::Turn(float _deg, VECTOR _axis)
 	trans_.quaRot = trans_.quaRot.AngleAxis(AsoUtility::Deg2RadF(_deg), _axis);
 }
 
+void Enemy::InitSkill(void)
+{
+	//ここにスキルの数分格納させる
+	skill_.emplace_back(SKILL_1);
+	skill_.emplace_back(SKILL_2);
+
+	//初期スキルを設定しておく
+	RandSkill();
+}
+
 void Enemy::Attack(void)
 {
-	//攻撃クールダウンカウンタ初期化
-	atkCdt_ = 0.0f;
+	//前方向
+	VECTOR dir = trans_.quaRot.GetForward();
 
-	//攻撃生成
-	CreateAtk();
+	//座標の設定
+	nowSkill_.pos_ = VAdd(trans_.pos, VScale(dir, 20.0f));
 }
 
-void Enemy::CreateAtk(void)
+void Enemy::RandSkill(void)
 {
-	//有効な攻撃を取得
-	ATK atk = GetValidAtk();
+	//スキルの数
+	size_t size = skill_.size();
 
-	
-}
+	//スキルの数分からランダムを取る
+	int rand = GetRand(size - 1);
 
-const Enemy::ATK Enemy::GetValidAtk(void)
-{
-	//現在存在する攻撃を取得
-	for (auto& atk : atk_)
-	{
-		//攻撃の時間が過ぎているなら
-		if (atk.cnt_ > atk.duration_ + atk.backlash_)
-		{
-			//カウンタを初期化してもう一度再利用する
-			atk.ResetCnt();
-
-			//TODO::新しい攻撃の情報を代入し、上書きする
-
-			return atk;
-		}
-	}
-
-	//新たな攻撃配列を生成
-	ATK newAtk;
-	
-	//カウンタの初期化
-	newAtk.ResetCnt();
-
-	//TODO::新しい攻撃の情報を代入し、上書きする
-
-	//新たに作った攻撃を配列に挿入
-	atk_.emplace_back(newAtk);
-
-	//作った攻撃を返す
-	return newAtk;
+	//ランダムでとってきた攻撃の種類を今から発動するスキルに設定
+	nowSkill_ = skill_[rand];
 }
--- Src/Object/Enemy.h
+++ Src/Object/Enemy.h
@@ -1,27 +1,44 @@
 #pragma once
 #include<vector>
+#include"../Utility/AsoUtility.h"
 #include "UnitBase.h"
 
 class Enemy : public UnitBase
 {
 public:
 
-	static constexpr float ATK_CDT = 50.0f;	//攻撃のクールダウンの時間
+	//アニメーション番号
+	static constexpr int ANIM_IDLE = 14;	//待機アニメーション
+	static constexpr int ANIM_WALK = 93;	//歩きアニメーション
+	static constexpr int ANIM_RUN = 54;		//走りアニメーション
+	static constexpr int ANIM_DAMAGE = 39;	//ダメージアニメーション
+	static constexpr int ANIM_DEATH = 71;	//やられアニメーション
+	static constexpr int ANIM_ENTRY = 74;	//出現アニメーション
+
+	static constexpr float ATK_CDT = 60.0f;		//攻撃のクールダウンの時間
+	static constexpr float ATK_BTC = 100.0f;	//攻撃の休憩時間
+
+	static constexpr float WALK_SPEED = 2.0f;	//歩きの速度
+	static constexpr float RUN_SPEED = 4.0f;	//走りの速度
+
+	static constexpr ATK SKILL_1 = { AsoUtility::VECTOR_ZERO,1.0f,60.0f,120.0f,0.0f };	//スキル１
+	static constexpr ATK SKILL_2 = { AsoUtility::VECTOR_ZERO,5.0f,180.0f,300.0f,0.0f };	//スキル２
 
 	//現在状態
 	enum class STATE
 	{
-		NML		//通常
-		,ATK	//攻撃
-		,DEAD	//死亡
+		NORMAL			//通常
+		,ALERT_ATTACK	//攻撃前(警告)
+		,ATTACK			//攻撃
+		,BREAK			//休憩
 		,MAX
 	};
 
 	//攻撃の種類
-	enum class ATK_PAT
-	{
+	//enum class ATK_PAT
+	//{
 
-	};
+	//};
 
 	//コンストラクタ
 	Enemy();
@@ -38,41 +55,94 @@ public:
 	//描画
 	void Draw(void)override;
 
+	//位置
+	const VECTOR GetPos(void)const override { return trans_.pos; }
+	//角度
+	const VECTOR GetRot(void)const override { return trans_.rot; }
+	//大きさ
+	const VECTOR GetScl(void)const override { return trans_.scl; }
+	//防御力
+	const float GetDef(void)const override { return def_; }
+
+	//攻撃クールダウン中かどうかを返す(true:攻撃クールダウン中)
+	const bool IsAtkCD(void)const { return atkCdt_ < ATK_CDT; }
+
+	const bool IsStun(void)const { return stunDef_ > stunDefMax_; }
+
+	//取得経験値を返す
+	const float GetExp(void) { return exp_; }
+
 	/// <summary>
 	/// ダメージ
 	/// </summary>
 	/// <param name="_damage">ダメージ量</param>
-	//void Damage(float _damage);
+	void Damage(const int _damage);
+
+	/// <summary>
+	/// 状態遷移
+	/// </summary>
+	/// <param name="_state">遷移する状態</param>
+	void ChangeState(const STATE _state) { state_ = _state; }
+
+protected:
+
+	//キャラ固有設定
+	virtual void SetParam(void);
+
+	//スキルの初期化
+	virtual void InitSkill(void);
 
 private:
 	
 	STATE state_;	//現在の状態
 
-	float atkCdt_;			//攻撃のクールダウンカウンタ
+	float atkCdt_;				//攻撃のクールダウンカウンタ
+	float atkBtc_;				//攻撃の休憩時間カウンタ
 
-	std::vector<ATK> atk_;	//攻撃情報
+	std::vector<ATK> skill_;	//スキルの種類
+	ATK nowSkill_;				//現在のスキル
+
+	float moveSpeed_;	//移動量
+
+	int stunDefMax_;	//気絶防御値の最大値
+	int stunDef_;		//気絶防御値
+
+	float exp_;		//取得経験値
+
+	//アニメーション番号の初期化
+	void InitAnimNum(void);
 
 	//更新(通常)
 	void UpdateNml(void);
+	//更新(攻撃警告)
+	void UpdateAlert(void);
 	//更新(攻撃)
 	void UpdateAtk(void);
-	//更新(死亡)
-	void UpdateDead(void);
+	//更新(休憩)
+	void UpdateBreak(void);
+
+	//索敵
+	void Search(void);
 
 	//移動
 	void Move(void);
-	//移動処理
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	/// <param name="_moveSpeed">移動距離</param>
+	/// <param name="_deg">移動方向(デグリー角)</param>
 	void ProcessMove(const float _moveSpeed, const float _deg);
-	//方向転換
+	/// <summary>
+	/// 方向転換
+	/// </summary>
+	/// <param name="_deg">回転方向(デグリー角)</param>
+	/// <param name="_axis">回転軸</param>
 	void Turn(const float _deg, const VECTOR _axis);
 
 	//敵の攻撃処理
 	void Attack(void);
+	//スキルのランダム生成
+	void RandSkill(void);
 
-	//攻撃の生成
-	void CreateAtk(void);
-
-	//有効な攻撃を取得
-	const ATK GetValidAtk(void);
 };
 
