02f7f5b421c7c431bbc1da7039cf06c1db4cf8f6
井東太陽 <dekirusunsun410@gmail.com>
2024-10-24 17:41:03 +0900

    コントローラー対応きちゃぁぁ！！！

--- Src/Object/PlayerBase.cpp
+++ Src/Object/PlayerBase.cpp
@@ -2,24 +2,32 @@
 #include"../Manager/SceneManager.h"
 #include"../Manager/ResourceManager.h"
 #include "PlayerBase.h"
-
 PlayerBase::PlayerBase(void)
 {
+	atk_.ResetCnt();
+	atk_.duration_ = FRAME_ATK_DURATION;
+	atk_.backlash_ = FRAME_ATK_BACKRASH;
+	atk_.pow_ = 0;
 }
 
 void PlayerBase::Destroy(void)
 {
 }
 
+void PlayerBase::SetParam(void)
+{
+
+}
+
 void PlayerBase::Init(void)
 {
 	dodgeCdt_ = DODGE_CDT_MAX;
-	color_ = 0xffffff;
-
-	
-	
 	speedMove_ = 0.0f;
+	ChangeControll(CNTL::KEYBOARD);
 
+	//アニメーション初期化
+	InitAnimNum();
+	ResetAnim(ANIM::WALK, SPEED_ANIM_IDLE);
 
 	//モデルの初期化
 	trans_.SetModel(
@@ -33,53 +41,43 @@ void PlayerBase::Init(void)
 		0.0f, AsoUtility::Deg2RadF(180.0f),
 		0.0f
 	);
-	//アニメーション初期化
-	animNum_[ANIM::IDLE] = IDLE_NUM;
-	animNum_[ANIM::WALK] = WALK_NUM;
-	animNum_[ANIM::DODGE] = DODGE_NUM;
-	animNum_[ANIM::UNIQUE_1] = ATK_NUM;
-	animNum_[ANIM::SKILL_1] = SKILL_NUM;
-	ResetAnim(ANIM::IDLE);
-
-	atk_.cnt_ = 0;
-	atk_.duration_ = FRAME_ATK_DURATION;
-	atk_.backlash_ = FRAME_ATK_BACKRASH;
-	atk_.pow_ = 0;
-
-	//スキル
-	
 
 	trans_.Update();
-
 }
 
 void PlayerBase::Update(void)
 {
 	//アニメーションの更新
 	Anim();
+
 	//モデルの更新
 	trans_.Update();
 
 	//停止状態の時のアニメーション
-	if (!IsMove() && !IsDodge() && !IsAtkAction() && anim_ != ANIM::IDLE)
+	if (!IsMove() && !IsDodge() && !IsAtkAction() &&!IsSkill(SKILL::ONE))
 	{
-		ResetAnim(ANIM::IDLE);
+		ResetAnim(ANIM::IDLE,SPEED_ANIM_IDLE);
 		speedMove_ = 0.0f;
 	}
 
 	//回避
 	Dodge();
 
+	switch (cntl_)
+	{
+	case PlayerBase::CNTL::KEYBOARD:
+		KeyBoardControl();
+		break;
+	case PlayerBase::CNTL::PAD:
+		GamePad();
+		break;
+	}
 
-	//コントロール系
-	KeyBoardControl();
 
 	//攻撃中か毎フレーム判定する
 	Attack();
 
-
-
-
+	Skill_1();
 
 }
 
@@ -91,14 +89,10 @@ void PlayerBase::Draw(void)
 
 void PlayerBase::Move(float _deg, VECTOR _axis)
 {
-	if (anim_!=ANIM::WALK&&!IsDodge()&&!IsAtkAction())
+	speedMove_ = SPEED_MOVE;
+	if (!IsDodge() && !IsAtkAction())
 	{
-		ResetAnim(ANIM::WALK);
-		speedMove_ = SPEED_MOVE;
-	}
-	
-	if (!atk_.IsAttack()&&!IsDodge() && !IsAtkAction())
-	{
+		ResetAnim(ANIM::WALK, SPEED_ANIM_RUN);
 		Turn(_deg, _axis);
 		VECTOR dir = trans_.GetForward();
 		//移動方向
@@ -108,9 +102,23 @@ void PlayerBase::Move(float _deg, VECTOR _axis)
 	}
 }
 
+void PlayerBase::InitAnimNum(void)
+{
+	animNum_.emplace(ANIM::IDLE, IDLE_NUM);
+	animNum_.emplace(ANIM::WALK, RUN_NUM);
+	animNum_.emplace(ANIM::DODGE, DODGE_NUM);
+	animNum_.emplace(ANIM::UNIQUE_1, ATK_NUM);
+	animNum_.emplace(ANIM::SKILL_1, SKILL_NUM);
+}
+
 void PlayerBase::KeyBoardControl(void)
 {
 	auto& ins = InputManager::GetInstance();
+	DINPUT_JOYSTATE input;
+	if (GetJoypadInputState(DX_INPUT_PAD1)&&CheckHitKeyAll() < 0)
+	{
+		ChangeControll(CNTL::PAD);
+	}
 	//前
 	if (ins.IsNew(KEY_INPUT_W))
 	{
@@ -135,33 +143,112 @@ void PlayerBase::KeyBoardControl(void)
 	{
 		speedMove_ = 0.0f;
 	}
+	
+
+	
+	
 
 	//攻撃（攻撃アニメーションのフレームが0以下だったらフレームを設定）
-	if (ins.IsTrgDown(KEY_INPUT_E)/*&&!atk_.IsAttack()*/)
+	if (ins.IsTrgDown(KEY_INPUT_E) && !atk_.IsAttack() && !IsDodge())
 	{
-		if (!atk_.IsAttack()&&!IsDodge()&&anim_!=ANIM::UNIQUE_1)
-		{
-			//アニメーション
-			ResetAnim(ANIM::UNIQUE_1);
-			//カウンタ開始
-			atk_.cnt_+=SceneManager::GetInstance().GetDeltaTime();
-		}
-
+		//アニメーション
+		ResetAnim(ANIM::UNIQUE_1, SPEED_ANIM_ATK);
+		//カウンタ開始
+		Count(atk_.cnt_);
 	}
 
-	if (ins.IsTrgDown(KEY_INPUT_Q)&&!IsAtkAction()&&!IsSkill(SKILL::TWO))
+	if (ins.IsTrgDown(KEY_INPUT_Q)&&!IsAtkAction()/*&&!IsSkill(SKILL::TWO)*/)
 	{
-		Skill_1();
-		ResetAnim(ANIM::SKILL_1);
+		Count(skillCnt_[SKILL::ONE]);
+		ResetAnim(ANIM::SKILL_1,SPEED_ANIM);
 	}
 
 	//回避
 	if (ins.IsTrgDown(KEY_INPUT_N) && !IsDodge() && !IsAtkAction()&&!IsCoolDodge())
 	{
-		ResetAnim(ANIM::DODGE);
-		frameDodge_+=SceneManager::GetInstance().GetDeltaTime();
+		ResetAnim(ANIM::DODGE,SPEED_ANIM_DODGE);
+		Count(frameDodge_);
 	}
 
+}
+
+void PlayerBase::GamePad(void)
+{
+	auto& ins = InputManager::GetInstance();
+	if (GetJoypadNum() == 0)
+	{
+		ChangeControll(CNTL::KEYBOARD);
+	}
+
+	// 左スティックの横軸
+	leftStickX_ = ins.GetJPadInputState(InputManager::JOYPAD_NO::PAD1).AKeyLX;
+
+	//縦軸
+	leftStickY_ = ins.GetJPadInputState(InputManager::JOYPAD_NO::PAD1).AKeyLY;
+
+	//方向決め
+	auto stickRad = static_cast<float>(atan2(static_cast<double>(leftStickY_), static_cast<double>(leftStickX_)));
+	stickDeg_ = static_cast<float>(AsoUtility::DegIn360(AsoUtility::Rad2DegF(stickRad) + 90.0f));
+	//前
+	if (leftStickY_ < -1)
+	{
+		Move(stickDeg_, AsoUtility::AXIS_Y);
+	}
+	//右
+	else if (leftStickX_ > 1)
+	{
+		Move(stickDeg_, AsoUtility::AXIS_Y);
+	}
+	//下
+	else if (leftStickY_ > 1)
+	{
+		Move(stickDeg_, AsoUtility::AXIS_Y);
+	}
+	//左
+	else if (leftStickX_ < -1)
+	{
+		Move(stickDeg_, AsoUtility::AXIS_Y);
+	}
+	else
+	{
+		speedMove_ = 0.0f;
+	}
+
+	//攻撃（攻撃アニメーションのフレームが0以下だったらフレームを設定）
+	if ( ins.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD1,InputManager::JOYPAD_BTN::RIGHT) && !atk_.IsAttack() && !IsDodge())
+	{
+		//アニメーション
+		ResetAnim(ANIM::UNIQUE_1, SPEED_ANIM_ATK);
+		//カウンタ開始
+		Count(atk_.cnt_);
+	}
+
+	//スキル
+	if (ins.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD1, InputManager::JOYPAD_BTN::TOP) && !IsAtkAction()/*&&!IsSkill(SKILL::TWO)*/)
+	{
+		Count(skillCnt_[SKILL::ONE]);
+		ResetAnim(ANIM::SKILL_1, SPEED_ANIM);
+	}
+
+	//回避
+	if (ins.IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD1, InputManager::JOYPAD_BTN::LEFT) && !IsDodge() && !IsAtkAction() && !IsCoolDodge())
+	{
+		ResetAnim(ANIM::DODGE, SPEED_ANIM_DODGE);
+		Count(frameDodge_);
+	}
+}
+
+void PlayerBase::ChangeControll(CNTL _cntl)
+{
+	//if (cntl_ == _cntl) { return; }
+	cntl_ = _cntl;
+	switch (cntl_)
+	{
+	case PlayerBase::CNTL::KEYBOARD:
+		break;
+	case PlayerBase::CNTL::PAD:
+		break;
+	}
 }
 
 void PlayerBase::DrawDebug(void)
@@ -169,7 +256,9 @@ void PlayerBase::DrawDebug(void)
 	//球体
 	//DrawSphere3D(trans_.pos, 20.0f, 8, 0x0, color_, true);
 	//値見る用
-	DrawFormatString(0, 0, 0xffffff, "FrameATK(%f)\nisAtk(%d)\nisBackSrash(%d)\nDodge(%d)", atk_.cnt_, atk_.IsAttack(),atk_.IsBacklash_(), frameDodge_);
+	DrawFormatString(0, 0, 0xffffff
+		, "FrameATK(%f)\nisAtk(%d)\nisBackSrash(%d)\nDodge(%f)\nSkill(%f)\nStick(%f)"
+		, atk_.cnt_, atk_.IsAttack(),atk_.IsBacklash(), frameDodge_,skillCnt_[SKILL::ONE],stickDeg_);
 }
 
 
@@ -186,15 +275,13 @@ void PlayerBase::Turn(float _deg, VECTOR _axis)
 
 void PlayerBase::Attack(void)
 {
-	if (atk_.IsAttack()||atk_.IsBacklash_())
+	if (atk_.IsAttack()||atk_.IsBacklash())
 	{
-		atk_.cnt_+=SceneManager::GetInstance().GetDeltaTime();
-		color_ = 0xff0000;
+		Count(atk_.cnt_);
 	}
 	else 
 	{
 		atk_.ResetCnt();
-		color_ = 0xffffff;
 	}
 	
 }
@@ -204,7 +291,7 @@ void PlayerBase::Dodge(void)
 	//ドッジフラグがtrueになったら
 	if (IsDodge()&&!IsCoolDodge())
 	{
-		frameDodge_+=SceneManager::GetInstance().GetDeltaTime();
+		Count(frameDodge_);
 		if (frameDodge_ < FRAME_DODGE_MAX)
 		{
 			VECTOR dir = trans_.GetForward();
@@ -220,7 +307,7 @@ void PlayerBase::Dodge(void)
 	}
 	else
 	{
-		dodgeCdt_ +=SceneManager::GetInstance().GetDeltaTime();
+		Count(dodgeCdt_);
 		ResetDodgeFrame();
 		color_ = 0xffffff;
 	}
@@ -229,11 +316,23 @@ void PlayerBase::Dodge(void)
 void PlayerBase::Skill_1(void)
 {
 	skillNum_ = "Skill_1";
-	skillCnt_[SKILL::ONE] += SceneManager::GetInstance().GetDeltaTime();
+	if (IsSkill(SKILL::ONE))
+	{
+		Count(skillCnt_[SKILL::ONE]);
+	}
+	else
+	{
+		skillCnt_[SKILL::ONE] = 0.0f;
+	}
 }
 
 void PlayerBase::Skill_2(void)
 {
 	skillNum_ = "Skill_2";
+}
+
+void PlayerBase::Damage(void)
+{
+	ResetAnim(ANIM::DAMAGE,SPEED_ANIM_RUN);
 }
 
--- Src/Object/PlayerBase.h
+++ Src/Object/PlayerBase.h
@@ -1,22 +1,12 @@
 #pragma once
 #include"../Utility/AsoUtility.h"
+#include"../Manager/SceneManager.h"
 #include<map>
 #include "UnitBase.h"
 class PlayerBase:
     public UnitBase
 {
 public:
-    // 移動スピード
-    static constexpr float SPEED_MOVE = 5.0f;
-    static constexpr float SPEED_DEG = 5.0f;
-    static constexpr float SPEED_DODGE = 15.0f;
-    static constexpr float FRAME_ATK_DURATION = 1.0f;
-    static constexpr float FRAME_ATK_BACKRASH = 0.3f;
-    static constexpr float FRAME_DODGE_MAX = 0.5f;
-    static constexpr float DODGE_CDT_MAX = 0.5f;
-    static constexpr float FRAME_SKILL1 = 0.5f;
-    static constexpr float FRAME_SKILL2 = 0.5f;
-
     //各アニメーション番号
     static constexpr int IDLE_NUM = 36;
     static constexpr int WALK_NUM = 72;
@@ -25,6 +15,25 @@ public:
     static constexpr int ATK_NUM = 1;
     static constexpr int SKILL_NUM = 2;
 
+    //アニメーションスピード
+    static constexpr int SPEED_ANIM_IDLE = 20.0f;
+    static constexpr int SPEED_ANIM_RUN = 80.0f;
+    static constexpr int SPEED_ANIM_DODGE = 30.0f;
+    static constexpr int SPEED_ANIM_ATK = 50.0f;
+
+
+    // 移動スピード
+    static constexpr float SPEED_MOVE = 5.0f;
+    static constexpr float SPEED_DEG = 5.0f;
+    static constexpr float SPEED_DODGE = 15.0f;
+    static constexpr float FRAME_ATK_DURATION = 0.2f;
+    static constexpr float FRAME_ATK_BACKRASH = 0.4f;
+    static constexpr float FRAME_DODGE_MAX = 0.43f;
+    static constexpr float DODGE_CDT_MAX = 0.5f;
+    static constexpr float FRAME_SKILL1 = 1.5f;
+    static constexpr float FRAME_SKILL2 = 0.5f;
+
+
 
 
 
@@ -32,6 +41,7 @@ public:
     PlayerBase(void);
     ~PlayerBase(void) = default;
     void Destroy(void)override;
+    virtual void SetParam(void);
     void Init(void)override;
     void Update(void)override;
     void Draw(void)override;
@@ -51,13 +61,38 @@ protected:
         ONE
         ,TWO
     };
+
+    enum class CNTL
+    {
+        KEYBOARD
+        ,PAD
+    };
+
+    //アニメNo初期化
+    void InitAnimNum(void);
+
+
     //球体の色
     unsigned int color_;
-    //操作系（コントローラー）
+    //操作系（キーボード)
     void KeyBoardControl(void);
+
+    //ゲームパッド
+    void GamePad(void);
+
+    CNTL cntl_;
+
+    //コントローラー変更用関数
+    void ChangeControll(CNTL _cntl);
 
     //デバッグ用関数
     void DrawDebug(void);
+
+    //カウント関数
+    void Count(float& _cnt)
+    { 
+        _cnt += SceneManager::GetInstance().GetDeltaTime(); 
+    }
 
 
    
@@ -84,11 +119,8 @@ protected:
 
     //攻撃処理
     void Attack(void);
-    bool IsAtkAction(void) { return atk_.IsAttack() || atk_.IsBacklash_(); }
+    bool IsAtkAction(void) { return atk_.IsAttack() || atk_.IsBacklash(); }
 
-
-    //攻撃フレーム
-    
 
     ATK atk_;
     
@@ -120,7 +152,7 @@ protected:
      /// </summary>
      /// <param name="_frameSkillNo">スキルフレーム最大値(今はスキル1か2)</param>
      /// <returns>スキル中/スキル中でない</returns>
-     bool IsSkill(SKILL _skill) { return skillCnt_[_skill] < FRAME_SKILL1; }
+     bool IsSkill(SKILL _skill) { return skillCnt_[_skill]>0.0f && skillCnt_[_skill] < FRAME_SKILL1; }
 
      //スキルクールタイム中フラグ
      bool IsSkillCool(void);
@@ -133,16 +165,16 @@ protected:
 
      std::string skillNum_;
 
-     //ゲッター
-     //--------------------------------------------
-     //位置
-     const VECTOR GetPos(void)const override { return trans_.pos; }
-     //角度
-     const VECTOR GetRot(void)const override { return trans_.rot; }
-     //大きさ
-     const VECTOR GetScl(void)const override { return trans_.scl; }
-     //防御力
-     const float GetDef(void)const override { return def_; };
+     //ダメージ関数
+     void Damage(void);
+
+     int leftStickX_;
+
+     int leftStickY_;
+
+     float stickDeg_;
+
+   
 private:
 };
 
