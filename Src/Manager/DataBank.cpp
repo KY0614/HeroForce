2f97fca1c6cf372fde59da0ebc3691cd863c4605
ratura3939 <158240290+ratura3939@users.noreply.github.com>
2024-11-11 15:21:34 +0900

    add Databank Output

--- Src/Manager/DataBank.cpp
+++ Src/Manager/DataBank.cpp
@@ -5,6 +5,7 @@ DataBank* DataBank::instance_ = nullptr;
 
 void DataBank::CreateInstance(void)
 {
+	
 }
 
 DataBank& DataBank::GetInstance(void)
@@ -49,7 +50,8 @@ void DataBank::Input(const SceneManager::ROLE _role, const int _playerNum)
 /// <summary>
 /// システムに関する数値入力 ※ユーザー数・ディスプレイ数以外で使用しないこと！！
 /// </summary>
-/// <param name="_info">設定対象</param>
+/// <param name="_info">設定対象
+/// (INFOのUSERとDISPLAYのみ)</param>
 /// <param name="_num">数値</param>
 void DataBank::Input(const INFO _info, const int _num)
 {
@@ -61,6 +63,40 @@ void DataBank::Input(const INFO _info, const int _num)
 
 	case INFO::USER_NUM:
 		userNum_ = _num;
+		break;
+	default:
+		//デバッグ用
+		assert("DataBankのInputの使用方法に間違いがあります。");
+		break;
+	}
+}
+
+/// <summary>
+/// プレイヤー情報の出力
+/// </summary>
+/// <param name="_playerNum">プレイヤー番号</param>
+/// <returns></returns>
+const DataBank::PLAYER_INFO DataBank::Output(const int _playerNum)
+{
+	return playerInfo_[_playerNum];
+}
+
+/// <summary>
+/// システムに関する数値出力（画面数・ユーザー数用）
+/// </summary>
+/// <param name="_info">対象
+/// (INFOのUSERとDISPLAYのみ)</param>
+/// <returns></returns>
+const int DataBank::Output(const INFO _info)
+{
+	switch (_info)
+	{
+	case INFO::DHISPLAY_NUM:
+		return displayNum_;
+		break;
+
+	case INFO::USER_NUM:
+		return userNum_;
 		break;
 	default:
 		//デバッグ用
