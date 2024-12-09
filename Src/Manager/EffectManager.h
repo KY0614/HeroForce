d4452e4c0e7601ab5b38b2ef84fbf78835f70496
ratura3939 <158240290+ratura3939@users.noreply.github.com>
2024-12-09 10:30:25 +0900

    add efcMethod

--- Src/Manager/EffectManager.h
+++ Src/Manager/EffectManager.h
@@ -1,4 +1,6 @@
 #pragma once
+#include<unordered_map>
+#include<string>
 class EffectManager
 {
 public:
@@ -7,7 +9,13 @@ public:
 		//使用するエフェクトを羅列
 	};
 
-private:
+	void Load(EFFECT& _efc,std::string _path);
+	void Play(void);
+	void Stop(void);
 
+private:
+	EFFECT type_;
+	std::unordered_map<EFFECT,int> effectRes_;
+	int playEffect_;
 };
 
