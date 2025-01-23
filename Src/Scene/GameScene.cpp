96516a6293a94eb569c92c58b2aae2eb48116296
ASOST\2316032 <keikocraft87@gmail.com>
2025-01-23 13:08:44 +0900

    fix

--- Src/Scene/GameScene.cpp
+++ Src/Scene/GameScene.cpp
@@ -190,7 +190,7 @@ void GameScene::Draw(void)
 void GameScene::Release(void)
 {
 	level_->Release();
-	stage_->Release();
+	stage_->Destroy();
 
 	SceneManager::GetInstance().ResetCameras();
 	SceneManager::GetInstance().ReturnSolo();
@@ -259,13 +259,13 @@ void GameScene::CollisionEnemy(void)
 			{
 				PlayerBase* p = playerMng_->GetPlayer(i);
 				//攻撃が当たる範囲 && プレイヤーが回避していないとき
-				if (col.IsHitAtk(*e, *p) && !p->IsDodge())
-				{
-					//ダメージ
-					p->Damage();
-					//使用した攻撃を判定終了に
-					e->SetIsHit(true);
-				}
+				//if (col.IsHitAtk(*e, *p) && !p->IsDodge())
+				//{
+				//	//ダメージ
+				//	p->Damage();
+				//	//使用した攻撃を判定終了に
+				//	e->SetIsHit(true);
+				//}
 			}
 		}
 	}
