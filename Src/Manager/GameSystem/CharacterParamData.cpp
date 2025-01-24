85ccbef31301a520d266a3f05ea529f806622be5
ASOST\2316032 <keikocraft87@gmail.com>
2025-01-24 17:01:44 +0900

    コメントアウトを外した

--- Src/Manager/GameSystem/CharacterParamData.cpp
+++ Src/Manager/GameSystem/CharacterParamData.cpp
@@ -1,26 +1,26 @@
 #include "../../Lib/nlohmann/json.hpp"
 #include "CharacterParamData.h"
 
-//using json = nlohmann::json;
-//CharacterParamData* CharacterParamData::instance_ = nullptr;
-//
-//void CharacterParamData::CreateInstance(void)
-//{
-//	if (instance_ == nullptr) {
-//		instance_ = new CharacterParamData();
-//	}
-//	instance_->Init();
-//}
-//
-//CharacterParamData& CharacterParamData::GetInstance(void)
-//{
-//	return *instance_;
-//}
-//
-//void CharacterParamData::Init(void)
-//{
-//}
-//
-//CharacterParamData::CharacterParamData()
-//{
-//}
+using json = nlohmann::json;
+CharacterParamData* CharacterParamData::instance_ = nullptr;
+
+void CharacterParamData::CreateInstance(void)
+{
+	if (instance_ == nullptr) {
+		instance_ = new CharacterParamData();
+	}
+	instance_->Init();
+}
+
+CharacterParamData& CharacterParamData::GetInstance(void)
+{
+	return *instance_;
+}
+
+void CharacterParamData::Init(void)
+{
+}
+
+CharacterParamData::CharacterParamData()
+{
+}
